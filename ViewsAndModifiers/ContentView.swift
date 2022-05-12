0ae3ef3de28132a14f05a87f494a739374c62246
Seth Singletary <singletaryseth8@gmail.com>
2022-05-12 10:27:11 -0400

    added

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -10,12 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
-    }
+        //.frame(maxWidth, .infinity, maxHeight: .infinity)
+          //  .background(.red)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.red)
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
